fd3707bd3a0547c05c1926f2abe4cfaff9835564
tanver68 <16101068@uap-bd.edu>
2020-12-18 12:43:01 +0600

    added shopping cart and cursor

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,6 +4,7 @@ import {faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
 
 const Product = (props) => {
+    //console.log(props); //console kore data dakhar jonno (shop.js er sob data akhane dakhar jonno)
 const {img,name,seller,price,stock} = props.product;
     return (
         <div className="product">
@@ -16,7 +17,9 @@ const {img,name,seller,price,stock} = props.product;
                 <p>by :{seller}</p>
                 <p>${price}</p>
                 <p><small>Only {stock} left in stock - order soon</small></p>
-                <button className="main-button"> <FontAwesomeIcon icon={faShoppingCart} /> Add curt</button>
+                <button className="main-button" onClick= {() => props.handleAddProduct(props.product)}> <FontAwesomeIcon icon={faShoppingCart} /> Add curt</button> 
+                
+                {/* akhane onclick er por arow function deya thn call korci karon (parameter pass hobe ) that means, click korar por product add hobe ,ja console e dakhte parbo...bt jdi arow function na deya call kortam tahole click korar age add hoye jayto karon () ata mane function call  */}
             </div>
             
         </div>
--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -7,16 +7,27 @@ import './Shop.css';
 const Shop = () => {
    
     const first10 = fakeData.slice(0,10);
-    const [products, serProducts] = useState(first10);
+    const [products,setProducts] = useState(first10);
+    const [cart,setCart] = useState([]);
+    const handleAddProduct = (product) =>{
+        console.log('product added',product);
+        const newCart = [...cart,product];
+        setCart(newCart)
+    }
     return (
         <div className="shop-container">
             <div className="product-container">
                 {
-                    products.map(pd => <Product product={pd}></Product> )
+                    products.map(pd => <Product 
+                        handleAddProduct = {handleAddProduct}
+                        product={pd}>
+
+                        </Product> )
                 }
             </div>
             <div className="cart-container">
-                  <h1>foshka</h1>
+                  <h3>This is cart</h3>
+                  <h5>Order Summary: {cart.length}</h5>
             </div>
            
         </div>
